d702b0fe23cd118523f6ca057d1617d8553ce9fa
Hugo van den Berg <hugo@tbdwebdesign.nl>
2015-09-23 21:31:18 +0200

    PEP8 compliant formatting, PEP257 compliant docstrings, removed illegal assignment of __name__ (done automatically)

--- timml/__init__.py
+++ timml/__init__.py
@@ -1,4 +1,4 @@
-'''
+"""
 Copyright (C), 2015, Mark Bakker.
 Mark Bakker, Delft University of Technology
 mark dot bakker at tudelft dot nl
@@ -6,13 +6,7 @@ mark dot bakker at tudelft dot nl
 TimML is a computer program for the simulation of steady-state
 multiaquifer flow with analytic elements and consists of a
 library of Python scripts and FORTRAN extensions.
-'''
-
-#--version number
-__name__='timml'
-__author__='Mark Bakker'
-from version import __version__
-
+"""
 # Import all classes and functions
 from ml import Model, Model3D
 from mlaquifer import Aquifer, PolygonInhom, CircleInhomData, EllipseInhomData
@@ -26,18 +20,11 @@ from mlpolyareasink import PolyAreaSink
 from mlinhom import MakeInhomPolySide, MakeInhomogeneity
 from mlcircinhom import CircleInhom
 from mltrace import traceline
-from mlutil import timcontour, timlayout, timtracelines, timvertcontour, capturezone
+from mlutil import timcontour, timlayout, timtracelines, timvertcontour, \
+    capturezone
 
-# Matlab utilities and some other ones
-# from mlutilities import *
-
-# Not needed
-# from mlholineelements import *
-# from mllinesinkgeneral import *
-
-# Not part of standard distribution
-# from mllake import *  # use of mllake requires installation of shapely
-# from TimMLmpl import *
+# Import version number
+from version import __version__
 
 
-
+__author__ = 'Mark Bakker'
