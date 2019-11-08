38b517fb6e8d1883f487b102907806414e6221b5
Hugo Lapré <hugo.lapre@brabantwater.nl>
2019-11-08 15:28:47 +0100

    Cleanup __init__.py
    - Sorted imports alphabetically by type
    - Apply black codestyle
    - Remove old commented out code

--- timml/__init__.py
+++ timml/__init__.py
@@ -1,4 +1,4 @@
-'''
+"""
 Copyright (C), 2015, Mark Bakker.
 Mark Bakker, Delft University of Technology
 mark dot bakker at tudelft dot nl
@@ -6,27 +6,34 @@ mark dot bakker at tudelft dot nl
 TimML is a computer program for the simulation of steady-state
 multiaquifer flow with analytic elements and consists of a
 library of Python scripts and FORTRAN extensions.
-'''
-#from __future__ import division, print_function, absolute_import
-
-#--version number
-__name__='timml'
-__author__='Mark Bakker'
-from .version import __version__
-
-# Import all classes and functions
-from .model import ModelMaq, Model3D, Model
-from .well import WellBase, Well, HeadWell
-from .constant import Constant, ConstantStar
-from .linesink import LineSinkBase, HeadLineSinkZero, HeadLineSink, LineSinkDitch, HeadLineSinkString, LineSinkDitchString
-from .linedoublet import ImpLineDoublet, ImpLineDoubletString, LeakyLineDoublet, LeakyLineDoubletString
+"""
 from .circareasink import CircAreaSink
-from .inhomogeneity import PolygonInhomMaq, BuildingPit
-from .inhomogeneity1d import StripInhomMaq, StripInhom3D
-from .uflow import Uflow
-from .trace import timtraceline, timtracelines
-from .linesink1d import LineSink1D, HeadLineSink1D
+from .constant import Constant, ConstantStar
+from .inhomogeneity import BuildingPit, PolygonInhomMaq
+from .inhomogeneity1d import StripInhom3D, StripInhomMaq
+from .linedoublet import (
+    ImpLineDoublet,
+    ImpLineDoubletString,
+    LeakyLineDoublet,
+    LeakyLineDoubletString,
+)
 from .linedoublet1d import ImpLineDoublet1D, LeakyLineDoublet1D
+from .linesink import (
+    HeadLineSink,
+    HeadLineSinkString,
+    HeadLineSinkZero,
+    LineSinkBase,
+    LineSinkDitch,
+    LineSinkDitchString,
+)
+from .linesink1d import HeadLineSink1D, LineSink1D
+from .model import Model, Model3D, ModelMaq
 from .stripareasink import StripAreaSink
+from .trace import timtraceline, timtracelines
+from .uflow import Uflow
+from .version import __version__
+from .well import HeadWell, Well, WellBase
 
+__name__ = "timml"
+__author__ = "Mark Bakker"
 __all__ = [s for s in dir() if not s.startswith("_")]
